63cc3ae7a8905ff7ec5fbd0a9197e80f48c02f1f
Agon Eous <Agoneous@gmail.com>
2019-10-28 16:52:01 +0200

    gitignore

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Agone\source\flutter\2019\barilan\bar_ilan"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Agone\source\repos\barilan"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
